0833f7cfbc99175893155b5b30e6c0066554c3fd
Baybhin Gurung <baybhin+github@gmail.com>
2026-03-05 12:55:30 -0500

    Add temporary implementation for jwt_decode

--- backend/helpers/jwt.py
+++ backend/helpers/jwt.py
@@ -1,7 +1,7 @@
 # All the JWT logic will be added here 
 from datetime import datetime, timedelta, timezone
 
-import os 
+import os, jwt
 
 
 def create_jwt_token(user_id: str):
@@ -11,3 +11,8 @@ def create_jwt_token(user_id: str):
         "exp": datetime.now(timezone.utc) + timedelta(hours=1) # Token expires in 1 hour
     }
     return jwt.encode(payload, os.getenv("JWT_SECRET"), algorithm="HS256")
+
+
+def decode_jwt_token():
+    pass 
+    # More to be implemented 
