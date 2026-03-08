36fe7dbefbcfe2e57e63bc4ad0816f21590ff5ba
Baybhin Gurung <baybhin+github@gmail.com>
2026-03-07 23:03:33 -0500

    Add updated controllers page

--- backend/controllers/user.py
+++ backend/controllers/user.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends 
 from dotenv import load_dotenv
-
 from classes.userdata import LoginData, SignUpData
+from sqlalchemy.orm import Session
+from backend.database.models import User 
+from backend.database.database import get_db
+
+import bcrypt 
 
 router = APIRouter()
 load_dotenv()
@@ -13,16 +17,27 @@ async def health_check():
 
 # Login endpoint
 @router.post("/api/login")
-async def login(data: LoginData):
+async def login(data: LoginData, db: Session = Depends(get_db)):
 
-    # data already becomes LoginData(email=email, passworwd=password)
+    # Check JWT
+
+    # Business logic checks 
+
     if len(data.password) < 8:
         raise HTTPException(status_code=422, detail="Password must be at least 8 characters long")
+
+    try:
+        user = db.query(User).filter(User.email == data.email).first()
+
+        if not user:
+            raise HTTPException(status_code=404, detail="Email not found")
+            
+        if not bcrypt.verify(user.password, data.password):
+            raise HTTPException(status_code=401, detail="Invalid Password")
+                
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
     
-    # More to be implemented 
-    pass 
-
-
 
 @router.post("/api/signup")
 async def signup(data: SignUpData):
