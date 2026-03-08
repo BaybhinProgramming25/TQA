b179e15ef8f02d3aa190caba986711e1245878b6
Baybhin Gurung <baybhin+github@gmail.com>
2026-03-07 23:08:18 -0500

    Modiy server file

--- backend/server.py
+++ backend/server.py
@@ -1,24 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from models.database import connect_mongo, close_mongo
 
-from controllers.qa import router as qa_router
 from controllers.user import router as user_router
-
+from backend.database.database import engine, get_db, Base
 import os
 
 @asynccontextmanager
-async def lifespan(app):
-    await connect_mongo()
-    yield
-    await close_mongo()
+async def lifespan(app: FastAPI):
+    os.makedirs("../mysql-data", exist_ok=True)
+    Base.metadata.create_all(bind=engine)
+    print('Tables Created!')
+    yield 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "http://localhost:5173",
-]
+origins = ["http://localhost:5173"]
 
 codespace_name = os.environ.get("CODESPACE_NAME")
 if codespace_name:
@@ -33,6 +30,6 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-app.include_router(qa_router)
+# app.include_router(qa_router)
 app.include_router(user_router)
 
