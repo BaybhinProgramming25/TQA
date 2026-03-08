14eec878d67d791c3d2e1fcc55b0e4a522e28515
Baybhin Gurung <baybhin+github@gmail.com>
2026-03-07 23:02:53 -0500

    Add SQLAlchemy database logic, still work in progress

---
+++ backend/database/database.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker, declarative_base 
+
+
+load_dotenv()
+import os 
+
+# SPECIFY database url
+DATABASE_URL= os.getenv("DATABASE_URL")
+
+# Make the engine
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
+
+
+# Gives a database session to each request 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    except:
+        db.close()
+
+
