74b414434806851f2a422b830e8f98d69be6910b
Baybhin Gurung <baybhin+github@gmail.com>
2026-03-25 12:52:54 -0400

    Add Message model used in database

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
 from datetime import datetime, timezone
 from database.database import Base
 
@@ -22,3 +22,14 @@ class Document(Base):
     filepath = Column(String(500), nullable=False)
     size = Column(Integer, nullable=False)
     uploaded_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+class Message(Base):
+
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_email = Column(String(50), ForeignKey("users.email"), nullable=False)
+    document_id = Column(Integer, ForeignKey("documents.id"), nullable=False)
+    sender = Column(String(10), nullable=False)
+    text = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
